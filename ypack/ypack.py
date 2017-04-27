9b1488ffe2243f3aa500f123e8817241f5c9cf7b
Yannic Kilcher <yannic.kilcher@gmail.com>
2017-04-27 10:51:15 +0200

    count params

--- ypack/ypack.py
+++ ypack/ypack.py
@@ -421,4 +421,4 @@ def count_params():
     "print number of trainable variables"
     size = lambda v: itt.reduce(lambda x, y: x*y, v.get_shape().as_list())
     n = sum(size(v) for v in tf.trainable_variables())
-    print "Model size: %dK" % (n//1000,)
+    print("Model size: %dK" % (n//1000,))
